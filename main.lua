fa451f3b8454872250a8c10b6742cd94c2b70050
Bobbyjones <davidantonioserrano66@gmail.com>
2015-10-16 19:01:35 -0400

    added a test map

--- main.lua
+++ main.lua
@@ -1,10 +1,38 @@
+local sti = require "libs.STI"
+local bump = require "libs.bump"
+
 function love.load()
 
 
+	-- Set Physics Meter
+	world = bump.newWorld(64)
+	-- Load map
+	map = sti.new("maps/test1.lua", { "bump" })
+	
+	map:bump_init(world)
+
+	--[[player = {
+
+}--]]
+	--world:add()
 
 end
 
+function love.update(dt)
+	map:update(dt)
+end
 
-function love.update( )
+function love.draw()
+	
+	love.graphics.setColor( 255,255,255,255 )
+	map:setDrawRange(0, 0, 800, 600)
+	map:draw()
 
+	love.graphics.setColor(255, 255, 0, 255)
+	map:bump_draw(world,collidables)
+
+end
+
+function love.resize(w, h)
+	map:resize(w, h)
 end
